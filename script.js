d4ed12cadba15d6c255df7914661947304e5e9c2
faaabi93 <fabian.baiersdoerfer@gmx.de>
2021-11-27 17:04:12 +0100

    added more functionality

--- script.js
+++ script.js
@@ -1,54 +1,96 @@
 const display = document.querySelector(".display");
-const deleteButton = document.querySelector(".delete")
+const opButtons = document.querySelectorAll(".control2");
+const deleteButton = document.querySelector(".delete");
 const numberButtons = document.querySelectorAll(".num");
+const plusMinusButton = document.querySelector(".plusminus");
+const errorField = document.querySelector(".error");
 
-let displayValue = 0;
+let displayVal = 0;
+let currentOp = undefined;
+let firstVal = undefined;
+
 
 /*--------------- Event Listeners -------------*/
+plusMinusButton.addEventListener("click", changePrefix);
+
 function addNumListeners(nodeList) {
     nodeList.forEach(element => element.addEventListener("click", updateDisplay))
 }
 
 function addDeleteListener(btn) {
-    btn.addEventListener("click", clearDisplay);
+    btn.addEventListener("click", clearDisplayAndVals);
+}
+
+function addOpListeners(nodeList) {
+    nodeList.forEach(element => element.addEventListener("click", setOperator))
+}
+
+function changePrefix() {
+    displayVal = displayVal * -1;
+    setDisplay(displayVal);
+}
+/*--------------- Op Function ---------------*/
+
+function setOperator(e) {
+    operator = this.dataset.calc;
+    if(operator !== "=") {
+        currentOp = operator
+        firstVal = displayVal;
+        clearDisplay();
+    }
+    if(operator === "=") {
+        asdf()
+        setDisplay(operate(currentOp, firstVal, displayVal));
+        firstVal = displayVal
+        currentOp = undefined;
+    }
+    asdf();
 }
 
 /*--------------- Display Function -------------*/
+function clearDisplayAndVals() {
+    clearDisplay()
+    firstVal = undefined;
+    currentOp = undefined;
+    errorField.textContent = "";
+}
+
 function clearDisplay() {
     display.textContent = "0";
+    displayVal = 0;
 }
 
 function updateDisplay(e) {
+    if(display.textContent.toString().includes(".") && this.dataset.calc === ".") {
+        return;
+    }
     if(display.textContent === "0") {
-        display.textContent = this.dataset.calc;
+        if(this.dataset.calc === ".") {
+            display.textContent = "0."
+        } else {
+            display.textContent = this.dataset.calc;
+        }
     } else if (display.textContent.length < 9) {
         addToDisplay(this.dataset.calc);
     }
-    displayValue = display.textContent;
-    console.log(displayValue);
+    displayVal = display.textContent;
+    console.log(displayVal);
 }
 
 function addToDisplay(num) {
     display.textContent += num;
+    displayVal = display.textContent;
+}
+
+function setDisplay(num) {
+    if(num.toString().length > 10) {
+        errorField.textContent = "sdfölasdfja"
+    }
+    display.textContent = num.toString().slice(0, 9);
+    displayVal = display.textContent;
 }
 
 /*--------------- Math Function -------------*/
-function add(n, m) {
-    return n + m;
-}
-
-function subtract(n, m) {
-    return n - m;
-}
-
-function multiply(n, m) {
-    return n * m;
-}
-
-function divide(n, m) {
-    return n / m;
-}
-
 function operate(op, n, m) {
     if(op === "+") return add(n, m);
     if(op === "-") return subtract(n, m);
@@ -56,6 +98,28 @@ function operate(op, n, m) {
     if(op === "/") return divide(n, m);
 }
 
+function add(n, m) {
+    return n*1 + m*1;
+}
+
+function subtract(n, m) {
+    return n*1 - m*1;
+}
+
+function multiply(n, m) {
+    return n*1 * m*1;
+}
+
+function divide(n, m) {
+    return n*1 / m*1;
+}
+/*--------------- Helper --------------*/
+function asdf() {
+    console.log(`DisplayVal ${displayVal}`)
+    console.log(`First ${firstVal}`)
+    console.log(`Op: ${currentOp}`)
+}
 /*--------------- Startup -------------*/
-addNumListeners(numberButtons);
+addNumListeners(numberButtons); 
 addDeleteListener(deleteButton);
+addOpListeners(opButtons)
